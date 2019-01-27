e8c94236e560bab59e503b056fd627ec741ef739
vjba <vjba@pm.me>
2019-01-27 17:09:40 +0000

    Updated example project for checkstyle

--- src/main/java/CO2015Group17/CW1/UserRepository.java
+++ src/main/java/CO2015Group17/CW1/UserRepository.java
@@ -2,8 +2,6 @@ package CO2015Group17.CW1;
 
 import org.springframework.data.repository.CrudRepository;
 
-import CO2015Group17.CW1.User;
-
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
