4e0bb32bd7a5a947dbeb7a248536d7f4b01d8ee3
vjba <vjba@pm.me>
2019-02-04 13:10:42 +0000

    Fixed entity name

---
+++ src/main/java/springData/domain/Position.java
@@ -0,0 +1,25 @@
+package springData.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
+
+@Entity(name = "position")
+public class Position {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    int positionid;
+
+    @Column(unique = true, nullable = false)
+    String positionname;
+}
