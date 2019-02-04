2cb1565e2fecc353973dbc5885b8db05b5b52b25
vjba <vjba@pm.me>
2019-02-04 13:08:06 +0000

    Added Organization schema

---
+++ src/main/java/springData/domain/Organization.java
@@ -0,0 +1,31 @@
+package springData.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
+
+@Entity(name = "Employees")
+public class Organization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    int organisationid;
+
+    @Column(unique = true, nullable = false)
+    String name;
+
+    @Column(unique = true, nullable = false)
+    String address;
+
+    @Column(unique = true, nullable = false)
+    String contactnumber;
+}
