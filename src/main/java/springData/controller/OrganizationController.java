d7a4c02b7d67d398fbe23462d9fa02b2ca2b5c55
dk969 <43640825+dk969@users.noreply.github.com>
2019-02-05 12:34:00 +0000

    Controller Imports

--- src/main/java/springData/controller/OrganizationController.java
+++ src/main/java/springData/controller/OrganizationController.java
@@ -1,3 +1,23 @@
+package springData.Controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import springData.domain.Organization;
+import springData.repository.OrganizationRepository;
+
 @Controller
 @RequestMapping ("/Organization")
 
--- src/main/java/springData/controller/ShiftsController.java
+++ src/main/java/springData/controller/ShiftsController.java
@@ -1,9 +1,29 @@
+package springData.Controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import springData.domain.Shifts;
+import springData.repository.ShiftsRepository;
+
 @Controller
 @RequestMapping("/shifts")
 
 public class ShiftsController {
    
-   @Autowired ShiftRepository shiftRepo;
+   @Autowired ShiftsRepository shiftRepo;
 
    @RequestMapping(value="/")
    public String (){
