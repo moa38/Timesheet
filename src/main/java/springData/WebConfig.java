98003a6eee51b16a9bba3f8d3d2f2ed44f2f7913
vjba <vjba@pm.me>
2019-02-27 02:39:47 +0000

    Update view mappings

--- src/main/java/springData/WebConfig.java
+++ src/main/java/springData/WebConfig.java
@@ -11,50 +11,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
    public void addViewControllers(ViewControllerRegistry registry) {
-      registry.addViewController("/").setViewName("/success");
+      registry.addViewController("/").setViewName("/login");
       registry.addViewController("/error").setViewName("error");
       registry.addViewController("/login").setViewName("login");
       registry.addViewController("/dashboard").setViewName("user/dashboard");
+      registry.addViewController("/add-shift").setViewName("user/add-shift");
+      registry.addViewController("/edit-shift").setViewName("user/edit-shift");
+      registry.addViewController("/submit-shift").setViewName("user/submit-shift");
+      registry.addViewController("/help").setViewName("help");
+      registry.addViewController("/account").setViewName("user/account");
+      registry.addViewController("/sign-up").setViewName("/sign-up");
    }
 
    // Handles HTTP GET requests for /resources/** by efficiently serving up static
    // resources in the ${webappRoot}/resources/ directory
+   // TODO
    @Override
    public void addResourceHandlers(ResourceHandlerRegistry registry) {
       registry.addResourceHandler("/static/**").addResourceLocations("/src/main/resources/static");
    }
-
-   // Java configuration equivalent to
-   // <mvc:default-servlet-handler/> in spring-servlet.xml
-   // used to use bootstrap when security is enabled
-   // @Override
-   // public void configureDefaultServletHandling(DefaultServletHandlerConfigurer
-   // configurer) {
-   // configurer.enable();
-   // }
-
-   // Resolves views selected for rendering by @Controllers to .jsp resources in
-   // the
-   // /WEB-INF/views directory
-   // @Bean
-   // public InternalResourceViewResolver viewResolver() {
-   // InternalResourceViewResolver viewResolver = new
-   // InternalResourceViewResolver();
-   // viewResolver.setViewClass(JstlView.class);
-   // viewResolver.setPrefix("/WEB-INF/views/");
-   // viewResolver.setSuffix(".jsp");
-   // viewResolver.setOrder(2);
-   // return viewResolver;
-   // }
-
-   // Alternative using ViewResolver
-   //
-   // public ViewResolver internalResourceViewResolver() {
-   // InternalResourceViewResolver bean = new InternalResourceViewResolver();
-   // bean.setViewClass(JstlView.class);
-   // bean.setPrefix("/WEB-INF/views/");
-   // bean.setSuffix(".jsp");
-   // return bean;
-   // }
 
 }
