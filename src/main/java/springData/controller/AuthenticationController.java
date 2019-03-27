f8713cc806e44b495403f1347ca41a35aa612590
Mohammed Ahmed - moa38 <mohde57@live.com>
2019-03-27 02:10:21 +0000

    Updated to show the current username

--- src/main/java/springData/controller/AuthenticationController.java
+++ src/main/java/springData/controller/AuthenticationController.java
@@ -1,7 +1,10 @@
 package springData.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,35 +19,17 @@ public class AuthenticationController {
       return "login";
    }
 
- /*  @RequestMapping(value="/login", method=RequestMethod.GET)
-   public String successLogin() { //HttpServletRequest httpServletRequest, Authentication authentication
-      User authUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-      springData.domain.User user = userRepo.findByUsername(authUser.getUsername());
-
-      return "/user/add-shift";
-   }*/
-
-   // @RequestMapping(value= "/login", method = RequestMethod.GET)
-   //public String printUser(Model model){
-   // AppUser user = (AppUser) SecurityContextHolder.getContext().getAuthentication.getPrincipal();
-   //String id = user.getUserId();
-   //model.addAttribute("Hi", id);
-   //return "dashboard";
-   //}
-   // this is what i have got for getting the users name, cant get it working atm
-   /*@RequestMapping (value = "/dashboard", method = RequestMethod.POST)
-    @ResponseBody
-      public String currentUser (Principal principal){
-       return principal.getuserId();
+   @RequestMapping (value = "/dashboard", method = RequestMethod.GET)
+   public String dashboard(Model model, Principal principal) {
+      model.addAttribute("name", principal.getName());
+      return "/user/dashboard";
    }
-    */
-   // this is the outline for the save and cancel in edit page- may need to be moved
 
    /*@RequestMapping(value="/edit", method=RequestMethod.POST, params="action=save")
-public ModelAndView save() {}
+   public ModelAndView save() {}
 
 
-@RequestMapping(value="/edit", method=RequestMethod.POST, params="action=cancel")
-public ModelAndView cancel() {}
+   @RequestMapping(value="/edit", method=RequestMethod.POST, params="action=cancel")
+   public ModelAndView cancel() {}
     */
 }
